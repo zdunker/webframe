b335e78acc4a676e0db46082de6f5427fe753280
sheng.li <sheng.li@ihandysoft.com>
2022-03-28 15:01:55 +0800

    adding Recovery() middleware to help protect server from crashing, user can choose whether to use it. Opt-in policy.

--- middlewares.go
+++ middlewares.go
@@ -1,7 +1,13 @@
 package webframe
 
 import (
+	"errors"
+	"fmt"
 	"log"
+	"net/http"
+	"runtime"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -14,4 +20,34 @@ func Logger() HandlerFunc {
 		// Calculate resolution time
 		log.Printf("[%d] %s in %v", c.StatusCode, c.Request.RequestURI, time.Since(t))
 	}
+}
+
+func Recovery() HandlerFunc {
+	return func(c *Context) {
+		defer func() {
+			if err := recover(); err != nil {
+				errMsg := fmt.Sprintf("%s", err)
+				log.Printf("%s\n\n", trace(errMsg))
+				c.ErrorResponse(http.StatusInternalServerError, errors.New("Internal Server Error"))
+			}
+		}()
+		c.Next()
+	}
+}
+
+func trace(msg string) string {
+	pcs := make([]uintptr, 32)
+	n := runtime.Callers(3, pcs)
+
+	var builder strings.Builder
+	builder.WriteString(msg)
+	for _, pc := range pcs[:n] {
+		fn := runtime.FuncForPC(pc)
+		file, line := fn.FileLine(pc)
+		builder.WriteString("\n\t")
+		builder.WriteString(file)
+		builder.WriteString(":")
+		builder.WriteString(strconv.Itoa(line))
+	}
+	return builder.String()
 }
